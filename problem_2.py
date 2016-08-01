ccb04e2b086992d8634c5d609d13c62213f24843
Eli Kirmayer <eli@pyze.com>
2016-08-01 15:58:14 -0700

    Added problem_3.py and updated problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -1,12 +1,18 @@
 #Find the sum of the even valued fib terms under 4 million
-# 5z^2 + 4 or 5z^2 − 4
+
+
+def isFib(n):
+    checkOne = (5 * n ** 2) + 4
+    checkTwo = (5 * n ** 2) - 4
+    if checkOne ** .5 % 1 == 0 or checkTwo ** .5 % 1 == 0:
+        return True
+    return False
+
+
 sum = 0
 for x in range(4000000):
     if x%2==0:
         if isFib(x):
+            sum += x
+print sum
 
-
-def isFib(n):
-    checkOne = 5 * n**2 + 4
-    checkTwo = 5 * n**2 − 4
-    if isinstance(sqrt(checkOne)n int)
